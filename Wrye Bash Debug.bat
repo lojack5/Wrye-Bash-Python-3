4f7fe0d6ee7fe5d53fab03e5d166544a29822c25
lojack5 <lojo.jacob@gmail.com>
2014-01-18 06:48:51 -0700

    fixup comment placement

--- Wrye Bash Debug.bat
+++ Wrye Bash Debug.bat
@@ -40,6 +40,7 @@ FOR /F "skip=%SKIPVAL% usebackq tokens=%TOKENS%*" %%i in (`%REGCMD%`) do IF x"%%
 :TOKENTESTDONE
 
 
+:: get python path from registry
 SET REGCMD=reg query HKLM\SOFTWARE\Python\PythonCore\3.3\InstallPath /ve
 %REGCMD% >NUL 2>&1 && FOR /F "skip=%SKIPVAL% usebackq tokens=%TOKENS%*" %%i in (`%REGCMD%`) do SET PYPATH=%%j
 IF NOT x"%PYPATH%"==x"" GOTO FOUND
@@ -48,7 +49,6 @@ SET REGCMD=reg query HKCU\SOFTWARE\Python\PythonCore\3.3\InstallPath /ve
 %REGCMD% >NUL 2>&1 && FOR /F "skip=%SKIPVAL% usebackq tokens=%TOKENS%*" %%i in (`%REGCMD%`) do SET PYPATH=%%j
 IF NOT x"%PYPATH%"==x"" GOTO FOUND
 
-:: get python path from registry
 SET REGCMD=reg query HKLM\SOFTWARE\Wow6432Node\Python\PythonCore\3.3\InstallPath /ve
 %REGCMD% >NUL 2>&1 && FOR /F "skip=%SKIPVAL% usebackq tokens=%TOKENS%*" %%i in (`%REGCMD%`) do SET PYPATH=%%j
 IF NOT x"%PYPATH%"==x"" GOTO FOUND
